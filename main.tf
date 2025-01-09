8e8d754aa491864cf31ad0e573dfdc08da6ed6bd
“windsleigh” <benjamincorderog@gmail.com>
2025-01-09 16:30:39 -0300

    added aws var def to variables.tf and replaced hardcoded values

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucketname
 }
+
--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
 variable "bucketname" {
     default = "aws-terraform-static-website-windsleigh"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
