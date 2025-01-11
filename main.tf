ce533ab700624c1ed89a1e013d264e06e3710d94
“windsleigh” <benjamincorderog@gmail.com>
2025-01-11 14:55:51 -0300

    enabled public read

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
