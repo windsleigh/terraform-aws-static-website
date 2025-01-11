8595adfcd6e0fa7953122a563f9e99ba1e38cac1
“windsleigh” <benjamincorderog@gmail.com>
2025-01-11 18:07:52 -0300

    static website initial config in s3

---
+++ website.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.bucket.arn}/*"
+      },
+    ]
+  })
+}
