7699b90c97e7cf132b961e1466bb374fffff9358
“windsleigh” <benjamincorderog@gmail.com>
2025-01-11 14:47:01 -0300

    changed bucket to public and permissions to only the owner

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucketname
 }
 
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
